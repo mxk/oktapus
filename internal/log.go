7bc056b04a9d2ecc2757c026f7a44d2ad609dab7
Maxim Khitrov <max@mxcrypt.com>
2018-01-14 16:00:21 -0500

    Remove glog
    Manual glog flag parsing was causing problems for 'go test'

--- internal/log.go
+++ internal/log.go
@@ -1,24 +1,43 @@
 package internal
 
 import (
-	"flag"
-
-	"github.com/golang/glog"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"sync"
 )
 
-// TODO: Implement proper logging
+var Log = log{os.Stderr}
 
-func init() {
-	flag.CommandLine.Parse([]string{"-logtostderr"}) // Configure glog
+var mu sync.Mutex
+var bufs = sync.Pool{New: func() interface{} {
+	return new(bytes.Buffer)
+}}
+
+type log struct{ w io.Writer }
+
+func (l log) D(format string, v ...interface{}) { l.out('D', format, v...) }
+func (l log) I(format string, v ...interface{}) { l.out('I', format, v...) }
+func (l log) W(format string, v ...interface{}) { l.out('W', format, v...) }
+func (l log) E(format string, v ...interface{}) { l.out('E', format, v...) }
+func (l log) F(format string, v ...interface{}) {
+	l.out('F', format, v...)
+	os.Exit(1)
 }
 
-type fmtFunc func(format string, args ...interface{})
-
-var Log = struct {
-	I, W, E, F fmtFunc
-}{
-	I: glog.Infof,
-	W: glog.Warningf,
-	E: glog.Errorf,
-	F: glog.Exitf,
+func (l log) out(lvl byte, format string, v ...interface{}) {
+	b := bufs.Get().(*bytes.Buffer)
+	b.Reset()
+	b.WriteByte('[')
+	b.WriteByte(lvl)
+	b.WriteString("] ")
+	fmt.Fprintf(b, format, v...)
+	if n := len(format); n > 0 && format[n-1] != '\n' {
+		b.WriteByte('\n')
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	l.w.Write(b.Bytes())
+	bufs.Put(b)
 }
