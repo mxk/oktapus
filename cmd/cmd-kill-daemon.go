514643464b07892cc60053426e89412b9ca726f1
Maxim Khitrov <max@mxcrypt.com>
2018-02-01 08:33:05 -0500

    Document 'kill-daemon' command

--- cmd/cmd-kill-daemon.go
+++ cmd/cmd-kill-daemon.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"bufio"
 	"flag"
 
 	"github.com/LuminalHQ/oktapus/daemon"
@@ -17,17 +18,35 @@ func init() {
 
 type killDaemon struct {
 	Name
-	active *bool
-	others bool
+	All    bool
+	Others bool
+}
+
+func (cmd *killDaemon) Help(w *bufio.Writer) {
+	writeHelp(w, `
+		A daemon process maintains persistent account credential and information
+		cache. A separate daemon is started for each unique authentication
+		context, which is derived from environment variables such as OKTA_ORG,
+		AWS_ACCESS_KEY_ID, etc.
+
+		Normally, there is no need to kill daemons. They terminate automatically
+		when no longer needed. This command forces a daemon to terminate.
+
+		The 'active' daemon is one that would be used for executing commands in
+		the current authentication context. If no options are specified, only
+		the active daemon is killed.
+	`)
+	accountSpecHelp(w)
 }
 
 func (cmd *killDaemon) FlagCfg(fs *flag.FlagSet) {
-	BoolPtrVar(fs, &cmd.active, "active", "Kill the active daemon")
-	fs.BoolVar(&cmd.others, "others", false, "Kill non-active daemons")
+	fs.BoolVar(&cmd.All, "all", false, "Kill all daemons")
+	fs.BoolVar(&cmd.Others, "others", false, "Kill non-active daemons")
 }
 
 func (cmd *killDaemon) Run(ctx *Ctx, args []string) error {
-	active := !cmd.others || (cmd.active != nil && *cmd.active)
-	daemon.Kill(ctx, active, cmd.others)
+	active := cmd.All || !cmd.Others
+	others := cmd.All || cmd.Others
+	daemon.Kill(ctx, active, others)
 	return nil
 }
