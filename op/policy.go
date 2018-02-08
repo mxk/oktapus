517ef01a9ed2f6479b85b15071c36065790376f3
Maxim Khitrov <max@mxcrypt.com>
2018-02-08 11:28:49 -0500

    Support entire IAM policy grammar

--- op/policy.go
+++ op/policy.go
@@ -1,8 +1,11 @@
 package op
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 )
@@ -11,39 +14,37 @@ const iamPolicyVersion = "2012-10-17"
 
 // Policy is an IAM policy document.
 type Policy struct {
-	Version   string
+	Version   string `json:",omitempty"`
+	ID        string `json:"Id,omitempty"`
 	Statement []*Statement
 }
 
 // NewAssumeRolePolicy returns an AssumeRole policy document.
 func NewAssumeRolePolicy(principal string) *Policy {
 	s := &Statement{
-		Effect:    "Deny",
-		Principal: &Principal{AWS: "*"},
-		Action:    Actions{"sts:AssumeRole"},
+		Effect:    "Allow",
+		Principal: new(Principal),
+		Action:    PolicyMultiVal{"sts:AssumeRole"},
 	}
-	p := &Policy{
-		Version:   iamPolicyVersion,
-		Statement: []*Statement{s},
+	if principal == "" {
+		s.Effect = "Deny"
+		principal = "*"
 	}
-	if principal != "" {
-		if isAWSAccountID(principal) {
-			principal = "arn:aws:iam::" + principal + ":root"
-		}
-		s.Effect = "Allow"
-		s.Principal.AWS = principal
-	}
-	return p
+	s.Principal.AWS = append(s.Principal.AWS, principal)
+	return &Policy{Version: iamPolicyVersion, Statement: []*Statement{s}}
 }
 
 // ParsePolicy decodes an IAM policy document.
 func ParsePolicy(doc *string) (*Policy, error) {
 	if doc == nil {
-		return nil, errors.New("missing policy document")
+		return nil, errors.New("policy: missing policy document")
 	}
 	p := new(Policy)
 	err := json.Unmarshal([]byte(*doc), &p)
 	if err != nil {
+		p = nil
+	} else if p.Version != "" && p.Version != iamPolicyVersion {
+		err = fmt.Errorf("policy: unsupported policy version %q", p.Version)
 		p = nil
 	}
 	return p, err
@@ -51,57 +52,108 @@ func ParsePolicy(doc *string) (*Policy, error) {
 
 // Doc returns JSON representation of policy p.
 func (p *Policy) Doc() *string {
-	b, err := json.Marshal(p)
-	if err != nil {
-		panic("policy encode error: " + err.Error())
+	if p.Version == "" {
+		p.Version = iamPolicyVersion
 	}
-	return aws.String(string(b))
+	// Buffer used to avoid HTML escaping
+	var buf bytes.Buffer
+	enc := json.NewEncoder(&buf)
+	enc.SetEscapeHTML(false)
+	if err := enc.Encode(p); err != nil {
+		panic("policy: encode error: " + err.Error())
+	}
+	return aws.String(strings.TrimSuffix(buf.String(), "\n"))
 }
 
 // Statement is an IAM policy statement.
 type Statement struct {
-	Effect    string     `json:",omitempty"`
-	Principal *Principal `json:",omitempty"`
-	Action    Actions    `json:",omitempty"`
+	SID          string                               `json:"Sid,omitempty"`
+	Effect       string                               `json:""`
+	Principal    *Principal                           `json:",omitempty"`
+	NotPrincipal *Principal                           `json:",omitempty"`
+	Action       PolicyMultiVal                       `json:",omitempty"`
+	NotAction    PolicyMultiVal                       `json:",omitempty"`
+	Resource     PolicyMultiVal                       `json:",omitempty"`
+	NotResource  PolicyMultiVal                       `json:",omitempty"`
+	Condition    map[string]map[string]PolicyMultiVal `json:",omitempty"`
 }
 
 // Principal specifies the entity to which a statement applies.
 type Principal struct {
-	AWS string
+	PrincipalMap
+	Any bool
 }
 
-// Actions specifies API calls that are allowed or denied by a statement.
-type Actions []string
+// PrincipalMap is a non-wildcard principal value.
+type PrincipalMap struct {
+	AWS       PolicyMultiVal `json:",omitempty"`
+	Federated PolicyMultiVal `json:",omitempty"`
+	Service   PolicyMultiVal `json:",omitempty"`
+}
+
+// NewAWSPrincipal returns a new Principal containing the specified AWS ids.
+func NewAWSPrincipal(ids ...string) *Principal {
+	return &Principal{PrincipalMap: PrincipalMap{AWS: PolicyMultiVal(ids)}}
+}
 
 // MarshalJSON implements json.Marshaler interface.
-func (a Actions) MarshalJSON() ([]byte, error) {
-	switch len(a) {
-	case 0:
-		if a == nil {
-			return []byte(`""`), nil
+func (p *Principal) MarshalJSON() ([]byte, error) {
+	if p.Any {
+		if len(p.AWS) != 0 || len(p.Federated) != 0 || len(p.Service) != 0 {
+			return nil, errors.New("policy: principal wildcard set and not set")
 		}
-		return []byte(`[]`), nil
-	case 1:
-		return json.Marshal(a[0])
-	default:
-		return json.Marshal([]string(a))
+		return []byte(`"*"`), nil
 	}
+	return json.Marshal(&p.PrincipalMap)
 }
 
 // UnmarshalJSON implements json.Unmarshaler interface.
-func (a *Actions) UnmarshalJSON(b []byte) error {
-	var s string
-	if err := json.Unmarshal(b, &s); err == nil {
-		if *a = (*a)[:0]; s != "" {
-			*a = append(*a, s)
-		}
-		return nil
-	}
-	v := []string(*a)[:0]
-	err := json.Unmarshal(b, &v)
+func (p *Principal) UnmarshalJSON(b []byte) error {
+	err := json.Unmarshal(b, &p.PrincipalMap)
 	if err != nil {
-		v = nil
+		var s string
+		if err = json.Unmarshal(b, &s); err == nil {
+			if s == "*" {
+				p.Any = true
+			} else {
+				err = fmt.Errorf("policy: invalid principal value %q", s)
+			}
+		}
 	}
-	*a = v
+	return err
+}
+
+// PolicyMultiVal is a JSON type that may be encoded either as a string or an
+// array, depending on the number of entries.
+type PolicyMultiVal []string
+
+// MarshalJSON implements json.Marshaler interface.
+func (v PolicyMultiVal) MarshalJSON() ([]byte, error) {
+	if len(v) == 0 {
+		return []byte(`[]`), nil
+	}
+	var buf bytes.Buffer
+	var err error
+	enc := json.NewEncoder(&buf)
+	enc.SetEscapeHTML(false)
+	if len(v) == 1 {
+		err = enc.Encode(v[0])
+	} else {
+		err = enc.Encode([]string(v))
+	}
+	return bytes.TrimSuffix(buf.Bytes(), []byte{'\n'}), err
+}
+
+// UnmarshalJSON implements json.Unmarshaler interface.
+func (v *PolicyMultiVal) UnmarshalJSON(b []byte) error {
+	var s string
+	err := json.Unmarshal(b, &s)
+	if err == nil {
+		*v = append((*v)[:0], s)
+	} else {
+		t := []string(*v)[:0]
+		err = json.Unmarshal(b, &t)
+		*v = t
+	}
 	return err
 }
