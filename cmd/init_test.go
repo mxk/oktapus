097c16bbbf4d71b70d818b7fe73078b278a880b6
Maxim Khitrov <max@mxcrypt.com>
2018-02-18 08:54:43 -0500

    Add init test

---
+++ cmd/init_test.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestInit(t *testing.T) {
+	cmd := newCmd("init").(*initCmd)
+	ctx := newCtx("1")
+	cmd.Spec = "test1,test2"
+	out, err := cmd.Call(ctx)
+	require.NoError(t, err)
+	want := []*resultsOutput{{
+		AccountID: "000000000001",
+		Name:      "test1",
+		Result:    "ERROR: already initialized",
+	}, {
+		AccountID: "000000000002",
+		Name:      "test2",
+		Result:    "OK",
+	}}
+	assert.Equal(t, want, out)
+}
