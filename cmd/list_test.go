e407fd5b53efcc9eee972e64501a2fe84128c448
Maxim Khitrov <max@mxcrypt.com>
2018-02-17 21:12:40 -0500

    Add list test

---
+++ cmd/list_test.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestList(t *testing.T) {
+	cmd := newCmd("list", "-refresh").(*list)
+	ctx := newCtx("1")
+	out, err := cmd.Call(ctx)
+	require.NoError(t, err)
+	want := []*listOutput{{
+		AccountID: "000000000001",
+		Name:      "test1",
+	}}
+	assert.Equal(t, want, out)
+}
