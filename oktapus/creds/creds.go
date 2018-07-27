e4b01ebd0e36ba08f617c973b0eef848ec420b2d
Maxim Khitrov <max@mxcrypt.com>
2018-07-27 10:46:40 -0400

    Add creds package

---
+++ oktapus/creds/creds.go
@@ -0,0 +1,202 @@
+package creds
+
+import (
+	"errors"
+	"path"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/LuminalHQ/cloudcover/x/arn"
+	"github.com/LuminalHQ/cloudcover/x/fast"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+)
+
+// ProxyProvider is the source name of Proxy credentials.
+const ProxyProvider = "ProxyCredentialsProvider"
+
+// ErrUnable is returned by Provider if the credentials do not satisfy the
+// requested validity duration after successful renewal.
+var ErrUnable = errors.New("creds: unable to satisfy minimum expiration time")
+
+// Ident contains the results of sts:GetCallerIdentity API call.
+type Ident struct {
+	arn.ARN
+	Account string
+	UserID  string
+}
+
+// Set updates identity information from call output.
+func (id *Ident) Set(out *sts.GetCallerIdentityOutput) {
+	id.ARN = arn.Value(out.Arn)
+	id.Account = aws.StringValue(out.Account)
+	id.UserID = aws.StringValue(out.UserId)
+}
+
+// SessName returns the RoleSessionName for the current identity.
+func (id Ident) SessName() string {
+	if i := strings.IndexByte(id.UserID, ':'); i != -1 {
+		return id.UserID[i+1:] // Current RoleSessionName or EC2 instance ID
+	}
+	if id.Type() == "user" {
+		return id.Name()
+	}
+	return id.UserID
+}
+
+// Proxy provides IAM role credentials via sts:AssumeRole API.
+type Proxy struct {
+	Client   sts.STS
+	Ident    Ident
+	SessName string
+}
+
+// NewProxy creates a new credentials proxy.
+func NewProxy(c *aws.Config) (*Proxy, error) {
+	p := &Proxy{Client: *sts.New(*c)}
+	out, err := p.Client.GetCallerIdentityRequest(nil).Send()
+	if err != nil {
+		return nil, err
+	}
+	p.Ident.Set(out)
+	p.SessName = p.Ident.SessName()
+	return p, nil
+}
+
+// Role returns the ARN for the specified account and role name. Account may be
+// empty to use the account of the client credentials.
+func (p *Proxy) Role(account, role string) arn.ARN {
+	if account == "" {
+		account = p.Ident.Account
+	}
+	return arn.New(p.Ident.Partition(), "iam", "", account, "role",
+		path.Clean("/"+role))
+}
+
+// AssumeRole returns a new Provider for the specified role. Default session
+// duration is used if d is zero.
+func (p *Proxy) AssumeRole(role arn.ARN, d time.Duration) *Provider {
+	in := &sts.AssumeRoleInput{
+		RoleArn:         arn.String(role),
+		RoleSessionName: aws.String(p.SessName),
+	}
+	if d != 0 {
+		in.DurationSeconds = aws.Int64(int64(d.Round(time.Second).Seconds()))
+	}
+	return p.Provider(in)
+}
+
+// Provider returns a new Provider that calls AssumeRole with the specified
+// input.
+func (p *Proxy) Provider(in *sts.AssumeRoleInput) *Provider {
+	return RenewableProvider(func() (aws.Credentials, error) {
+		out, err := p.Client.AssumeRoleRequest(in).Send()
+		cr := aws.Credentials{Source: ProxyProvider}
+		if err == nil {
+			src := out.Credentials
+			cr.AccessKeyID = aws.StringValue(src.AccessKeyId)
+			cr.SecretAccessKey = aws.StringValue(src.SecretAccessKey)
+			cr.SessionToken = aws.StringValue(src.SessionToken)
+			cr.CanExpire = true
+			cr.Expires = aws.TimeValue(src.Expiration)
+		}
+		return cr, err
+	})
+}
+
+// RenewFunc renews client credentials. CanExpire and Expires fields control
+// error caching if an error is returned. If CanExpire is false, Provider
+// automatically caches the error for a limited amount of time.
+type RenewFunc func() (aws.Credentials, error)
+
+// Provider is a replacement for aws.SafeCredentialsProvider. It allows clients
+// to ensure credential validity for a period of time in the future, and caches
+// error information to avoid unnecessary network traffic. Provider values must
+// not be copied.
+type Provider struct {
+	cr    atomic.Value
+	mu    sync.Mutex
+	renew RenewFunc
+}
+
+// RenewableProvider returns an aws.CredentialsProvider that automatically
+// renews its credentials as they expire.
+func RenewableProvider(fn RenewFunc) *Provider {
+	p := &Provider{renew: fn}
+	p.cr.Store((*creds)(nil))
+	return p
+}
+
+// Retrieve implements aws.CredentialsProvider.
+func (p *Provider) Retrieve() (aws.Credentials, error) {
+	return p.ensure(time.Minute)
+}
+
+// Creds returns currently cached credentials and error without renewal.
+func (p *Provider) Creds() (aws.Credentials, error) {
+	if cr := p.cr.Load().(*creds); cr != nil {
+		return cr.Credentials, cr.err
+	}
+	return aws.Credentials{}, nil
+}
+
+// Ensure ensures that credentials will remain valid for the specified duration,
+// renewing them if necessary. A negative duration forces unconditional renewal.
+// ErrUnable is returned if the validity period cannot be satisfied.
+func (p *Provider) Ensure(d time.Duration) error {
+	_, err := p.ensure(d)
+	return err
+}
+
+func (p *Provider) ensure(d time.Duration) (aws.Credentials, error) {
+	cr := p.cr.Load().(*creds)
+	if cr.keepCurrent(d) {
+		return cr.Credentials, cr.err
+	}
+	if p.renew != nil {
+		p.mu.Lock()
+		defer p.mu.Unlock()
+		if cr = p.cr.Load().(*creds); cr.keepCurrent(d) {
+			return cr.Credentials, cr.err
+		}
+		cr = new(creds)
+		cr.Credentials, cr.err = p.renew()
+		if cr.err != nil && !cr.CanExpire {
+			cr.CanExpire = true
+			if t := fast.Time(); aws.IsErrorThrottle(cr.err) {
+				cr.Expires = t.Add(2 * time.Minute)
+			} else {
+				cr.Expires = t.Add(2 * time.Hour)
+			}
+		}
+		p.cr.Store(cr)
+	} else if cr == nil {
+		return aws.Credentials{}, ErrUnable
+	}
+	if d < 0 {
+		d = 0
+	}
+	if cr.keepCurrent(d) || cr.err != nil {
+		return cr.Credentials, cr.err
+	}
+	return cr.Credentials, ErrUnable
+}
+
+// creds extends aws.Credentials with a persistent error.
+type creds struct {
+	aws.Credentials
+	err error
+}
+
+func (cr *creds) keepCurrent(d time.Duration) bool {
+	if cr == nil || d < 0 {
+		return false
+	}
+	if !cr.CanExpire {
+		return true
+	}
+	t := fast.Time()
+	return cr.Expires.After(t.Add(d)) || (cr.err != nil && cr.Expires.After(t))
+}
---
+++ oktapus/creds/creds_test.go
@@ -0,0 +1,167 @@
+package creds
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/LuminalHQ/cloudcover/x/awsmock"
+	"github.com/LuminalHQ/cloudcover/x/fast"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSessName(t *testing.T) {
+	assert.Equal(t, "y",
+		Ident{UserID: "x:y"}.SessName())
+	assert.Equal(t, "alice",
+		Ident{ARN: "arn:aws:iam::000000000000:user/alice"}.SessName())
+	assert.Equal(t, "000000000001",
+		Ident{ARN: "arn:aws:iam::000000000000:root", UserID: "000000000001"}.SessName())
+}
+
+func TestProxy(t *testing.T) {
+	now := fast.Time()
+	cfg := awsmock.Config(func(r *aws.Request) {
+		switch out := r.Data.(type) {
+		case *sts.GetCallerIdentityOutput:
+			out.Account = aws.String("000000000000")
+			out.Arn = aws.String("arn:aws:iam::000000000000:user/alice")
+		case *sts.AssumeRoleOutput:
+			want := &sts.AssumeRoleInput{
+				RoleArn:         aws.String("arn:aws:iam::000000000000:role/testrole"),
+				RoleSessionName: aws.String("alice"),
+			}
+			assert.Equal(t, want, r.Params)
+			out.Credentials = &sts.Credentials{
+				AccessKeyId:     aws.String("tempkey"),
+				SecretAccessKey: aws.String("secret"),
+				SessionToken:    aws.String("token"),
+				Expiration:      aws.Time(now.Add(time.Hour)),
+			}
+		}
+	})
+	p, err := NewProxy(&cfg)
+	require.NoError(t, err)
+
+	cr, err := p.AssumeRole(p.Role("", "testrole"), 0).Retrieve()
+	require.NoError(t, err)
+	want := aws.Credentials{
+		AccessKeyID:     "tempkey",
+		SecretAccessKey: "secret",
+		SessionToken:    "token",
+		Source:          ProxyProvider,
+		CanExpire:       true,
+		Expires:         now.Add(time.Hour),
+	}
+	assert.Equal(t, want, cr)
+}
+
+func TestProvider(t *testing.T) {
+	fast.MockTime(fast.Time())
+	defer fast.MockTime(time.Time{})
+
+	// Permanent creds
+	calls := 0
+	p := RenewableProvider(func() (aws.Credentials, error) {
+		calls++
+		return aws.Credentials{AccessKeyID: "key1"}, nil
+	})
+	cr, err := p.Retrieve()
+	assert.Equal(t, 1, calls)
+	assert.NoError(t, err)
+	assert.Equal(t, aws.Credentials{AccessKeyID: "key1"}, cr)
+
+	assert.NoError(t, p.Ensure(24*time.Hour))
+	assert.Equal(t, 1, calls)
+
+	assert.NoError(t, p.Ensure(-1))
+	assert.Equal(t, 2, calls)
+
+	cr, err = p.Creds()
+	assert.NoError(t, err)
+	assert.Equal(t, aws.Credentials{AccessKeyID: "key1"}, cr)
+
+	// Temporary creds
+	calls = 0
+	p = RenewableProvider(func() (aws.Credentials, error) {
+		calls++
+		return aws.Credentials{
+			AccessKeyID: "key2",
+			CanExpire:   true,
+			Expires:     fast.Time().Add(time.Hour),
+		}, nil
+	})
+	cr, err = p.Creds()
+	assert.Equal(t, 0, calls)
+	assert.NoError(t, err)
+	assert.Equal(t, aws.Credentials{}, cr)
+
+	assert.NoError(t, p.Ensure(time.Hour-1))
+	assert.Equal(t, 1, calls)
+
+	cr, err = p.Retrieve()
+	assert.Equal(t, 1, calls)
+	assert.NoError(t, err)
+	want := aws.Credentials{
+		AccessKeyID: "key2",
+		CanExpire:   true,
+		Expires:     fast.Time().Add(time.Hour),
+	}
+	assert.Equal(t, want, cr)
+
+	// Renewal
+	fast.MockTime(fast.Time().Add(30 * time.Minute))
+	assert.NoError(t, p.Ensure(30*time.Minute))
+	assert.Equal(t, 2, calls)
+
+	cr, err = p.Creds()
+	assert.NoError(t, err)
+	want.Expires = fast.Time().Add(time.Hour)
+	assert.Equal(t, want, cr)
+
+	// Invalid duration
+	assert.Equal(t, ErrUnable, p.Ensure(time.Hour))
+	assert.Equal(t, 3, calls)
+
+	assert.NoError(t, p.Ensure(30*time.Minute))
+	assert.Equal(t, 3, calls)
+
+	cr, err = p.Creds()
+	assert.NoError(t, err)
+	assert.Equal(t, want, cr)
+
+	// Error caching
+	e := errors.New("temporary error")
+	calls = 0
+	p = RenewableProvider(func() (cr aws.Credentials, err error) {
+		if calls++; calls == 1 {
+			err = e
+		}
+		return
+	})
+	cr, err = p.Retrieve()
+	assert.Equal(t, 1, calls)
+	assert.Equal(t, e, err)
+	want = aws.Credentials{
+		CanExpire: true,
+		Expires:   fast.Time().Add(2 * time.Hour),
+	}
+	assert.Equal(t, want, cr)
+
+	assert.Equal(t, e, p.Ensure(time.Hour))
+	assert.Equal(t, 1, calls)
+
+	fast.MockTime(fast.Time().Add(2*time.Hour - 1))
+	assert.Equal(t, e, p.Ensure(24*time.Hour))
+	assert.Equal(t, 1, calls)
+
+	fast.MockTime(fast.Time().Add(1))
+	assert.NoError(t, p.Ensure(0))
+	assert.Equal(t, 2, calls)
+
+	cr, _ = p.Creds()
+	assert.Equal(t, aws.Credentials{}, cr)
+}
