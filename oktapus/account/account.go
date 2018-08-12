c82b4c78cd96089909d7809f535025ccc264cbc4
Maxim Khitrov <max@mxcrypt.com>
2018-08-12 07:49:45 -0400

    Simplify account.Directory API

--- oktapus/account/account.go
+++ oktapus/account/account.go
@@ -3,35 +3,85 @@ package account
 import (
 	"bufio"
 	"bytes"
-	"errors"
 	"fmt"
 	"os"
-	"sort"
 	"time"
 
-	"github.com/LuminalHQ/cloudcover/oktapus/creds"
 	"github.com/LuminalHQ/cloudcover/x/arn"
-	"github.com/LuminalHQ/cloudcover/x/fast"
 	"github.com/LuminalHQ/cloudcover/x/region"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/awserr"
 	orgs "github.com/aws/aws-sdk-go-v2/service/organizations"
-	"github.com/aws/aws-sdk-go-v2/service/sts"
 )
 
-// ErrNoOrg indicates that the current account is not part of an organization.
-var ErrNoOrg = errors.New("account: not in organization")
+var errNoOrg = awserr.New(orgs.ErrCodeAWSOrganizationsNotInUseException,
+	"organizations api not supported in current partition", nil)
 
-// ErrNotMaster indicates that the current account is not allowed to make
-// certain API calls because it is not the organization master.
-var ErrNotMaster = errors.New("account: not organization master")
+// IsID returns true if id is a valid AWS account ID.
+func IsID(id string) bool {
+	if len(id) != 12 {
+		return false
+	}
+	for _, c := range []byte(id) {
+		if c-'0' > 9 {
+			return false
+		}
+	}
+	return true
+}
+
+// IsErrorNoOrg returns true if err indicates that Organizations API is not
+// available for the current account.
+func IsErrorNoOrg(err error) bool {
+	e, ok := err.(awserr.Error)
+	return ok && e.Code() == orgs.ErrCodeAWSOrganizationsNotInUseException
+}
+
+// LoadAliases loads account aliases from a file. The file should contain one
+// alias per line in the format "<partition> <account-id> <alias>". Empty lines
+// and lines beginning with '#' are ignored.
+func LoadAliases(file, partition string) (map[string]string, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	s, ln := bufio.NewScanner(f), 0
+	m := make(map[string]string)
+	for s.Scan() {
+		f := bytes.Fields(s.Bytes())
+		if ln++; len(f) != 3 {
+			if len(f) == 0 || f[0][0] == '#' {
+				continue
+			}
+			return nil, fmt.Errorf("account: invalid alias record at %s:%d",
+				file, ln)
+		}
+		if string(f[0]) != partition {
+			continue
+		}
+		id, alias := string(f[1]), string(f[2])
+		if !IsID(id) {
+			return nil, fmt.Errorf("account: invalid account id at %s:%d",
+				file, ln)
+		}
+		if alias == "" {
+			return nil, fmt.Errorf("account: invalid account alias at %s:%d",
+				file, ln)
+		}
+		m[id] = alias
+	}
+	if err = s.Err(); err != nil || len(m) == 0 {
+		m = nil
+	}
+	return m, err
+}
 
 // Info contains account information.
 type Info struct {
+	arn.ARN
 	ID         string
-	ARN        arn.ARN
 	Name       string
-	Alias      string
 	Email      string
 	Status     orgs.AccountStatus
 	JoinMethod orgs.AccountJoinedMethod
@@ -40,237 +90,86 @@ type Info struct {
 
 // Set updates account information.
 func (ac *Info) Set(src *orgs.Account) {
-	*ac = Info{
-		ID:         aws.StringValue(src.Id),
-		ARN:        arn.Value(src.Arn),
-		Name:       aws.StringValue(src.Name),
-		Alias:      ac.Alias,
-		Email:      aws.StringValue(src.Email),
-		Status:     src.Status,
-		JoinMethod: src.JoinedMethod,
-		JoinTime:   aws.TimeValue(src.JoinedTimestamp),
-	}
-}
-
-// DisplayName returns the account alias, if set, or its name.
-func (ac *Info) DisplayName() string {
-	if ac.Alias != "" {
-		return ac.Alias
-	}
-	return ac.Name
+	ac.ARN = arn.Value(src.Arn)
+	ac.ID = aws.StringValue(src.Id)
+	ac.Name = aws.StringValue(src.Name)
+	ac.Email = aws.StringValue(src.Email)
+	ac.Status = src.Status
+	ac.JoinMethod = src.JoinedMethod
+	ac.JoinTime = aws.TimeValue(src.JoinedTimestamp)
 }
 
 // Org contains organization information.
 type Org struct {
-	ARN         arn.ARN
-	FeatureSet  orgs.OrganizationFeatureSet
+	arn.ARN
 	ID          string
-	MasterARN   arn.ARN
+	FeatureSet  orgs.OrganizationFeatureSet
+	Master      arn.ARN
 	MasterEmail string
 	MasterID    string
 }
 
 // Set updates organization information.
 func (o *Org) Set(src *orgs.Organization) {
-	*o = Org{
-		ARN:         arn.Value(src.Arn),
-		FeatureSet:  src.FeatureSet,
-		ID:          aws.StringValue(src.Id),
-		MasterARN:   arn.Value(src.MasterAccountArn),
-		MasterEmail: aws.StringValue(src.MasterAccountEmail),
-		MasterID:    aws.StringValue(src.MasterAccountId),
-	}
+	o.ARN = arn.Value(src.Arn)
+	o.ID = aws.StringValue(src.Id)
+	o.FeatureSet = src.FeatureSet
+	o.Master = arn.Value(src.MasterAccountArn)
+	o.MasterEmail = aws.StringValue(src.MasterAccountEmail)
+	o.MasterID = aws.StringValue(src.MasterAccountId)
 }
 
-// Directory provides account information using AWS Organizations API and/or an
-// alias file.
+// Directory retrieves account information from AWS Organizations API.
 type Directory struct {
-	client   *orgs.Organizations
-	ident    creds.Ident
-	org      Org
-	aliases  map[string]string
-	accounts map[string]*Info
+	Client   orgs.Organizations
+	Org      Org
+	Accounts map[string]*Info
 }
 
 // NewDirectory returns a new account directory.
 func NewDirectory(cfg *aws.Config) *Directory {
-	return &Directory{client: orgs.New(*cfg)}
+	return &Directory{Client: *orgs.New(*cfg)}
 }
 
-// Init initializes organization and client identity information.
+// Init initializes organization information.
 func (d *Directory) Init() error {
-	part := region.Partition(d.client.Config.Region)
-	if region.Subset(part, orgs.ServiceName) == nil {
-		return ErrNoOrg
+	if d.noOrg() {
+		return errNoOrg
 	}
-	var stsErr error
-	err := fast.Call(
-		func() error {
-			out, err := d.client.DescribeOrganizationRequest(nil).Send()
-			if err == nil {
-				d.org.Set(out.Organization)
-			}
-			return err
-		},
-		func() error {
-			c := sts.New(d.client.Config)
-			out, err := c.GetCallerIdentityRequest(nil).Send()
-			if err == nil {
-				d.ident.Set(out)
-			}
-			stsErr = err
-			return nil
-		},
-	)
+	out, err := d.Client.DescribeOrganizationRequest(nil).Send()
 	if err == nil {
-		err = stsErr
-	} else if e, ok := err.(awserr.Error); ok &&
-		e.Code() == orgs.ErrCodeAWSOrganizationsNotInUseException {
-		err = ErrNoOrg
+		d.Org.Set(out.Organization)
 	}
 	return err
 }
 
 // Refresh updates account information.
 func (d *Directory) Refresh() error {
-	if d.ident.Account == "" {
-		if err := d.Init(); err != nil {
-			return err
-		}
+	if d.noOrg() {
+		return errNoOrg
 	}
-	if d.ident.Account != d.org.MasterID {
-		if d.org.MasterID == "" {
-			return ErrNoOrg
-		}
-		return ErrNotMaster
-	}
-	acs := make(map[string]*Info, len(d.accounts))
-	r := d.client.ListAccountsRequest(nil)
+	m := make(map[string]*Info, len(d.Accounts))
+	r := d.Client.ListAccountsRequest(nil)
 	p := r.Paginate()
 	for p.Next() {
 		out := p.CurrentPage()
+		buf := make([]Info, len(out.Accounts))
 		for i := range out.Accounts {
-			src := &out.Accounts[i]
-			ac := d.accounts[aws.StringValue(src.Id)]
-			if ac == nil {
-				ac = new(Info)
-			}
-			ac.Set(src)
-			acs[ac.ID] = ac
+			ac := &buf[i]
+			ac.Set(&out.Accounts[i])
+			m[ac.ID] = ac
 		}
 	}
 	err := p.Err()
 	if err == nil {
-		d.accounts = acs
-		d.applyAliases()
+		d.Accounts = m
 	}
 	return err
 }
 
-// Ident returns the identity of client credentials.
-func (d *Directory) Ident() creds.Ident { return d.ident }
-
-// Org returns organization information. The zero value is returned if the
-// current account is not part of an organization.
-func (d *Directory) Org() Org { return d.org }
-
-// LoadAliases updates account information from an alias file.
-func (d *Directory) LoadAliases(file string) error {
-	f, err := os.Open(file)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	part := []byte(region.Partition(d.client.Config.Region))
-	s, ln := bufio.NewScanner(f), 0
-	aliases := make(map[string]string)
-	for s.Scan() {
-		f := bytes.Fields(s.Bytes())
-		if ln++; len(f) != 3 {
-			if len(f) == 0 || f[0][0] == '#' {
-				continue
-			}
-			return fmt.Errorf("account: invalid record at %s:%d", file, ln)
-		}
-		if !bytes.Equal(f[0], part) {
-			continue
-		}
-		id, alias := string(f[1]), string(f[2])
-		if !isAccountID(id) {
-			return fmt.Errorf("account: invalid account id at %s:%d", file, ln)
-		}
-		if aliases[id] != "" {
-			return fmt.Errorf("account: duplicate account id %s:%d", file, ln)
-		}
-		if alias == "" {
-			return fmt.Errorf("account: invalid account alias at %s:%d", file, ln)
-		}
-		aliases[id] = alias
-	}
-	if err = s.Err(); err == nil {
-		if d.aliases == nil {
-			d.aliases = aliases
-		} else {
-			for id, alias := range aliases {
-				d.aliases[id] = alias
-			}
-		}
-		d.applyAliases()
-	}
-	return err
-}
-
-// Account returns information for one account ID.
-func (d *Directory) Account(id string) *Info { return d.accounts[id] }
-
-// Accounts returns all known accounts sorted by ID.
-func (d *Directory) Accounts() []*Info {
-	acs := make([]*Info, 0, len(d.accounts))
-	for _, ac := range d.accounts {
-		acs = append(acs, ac)
-	}
-	sort.Slice(acs, func(i, j int) bool { return acs[i].ID < acs[j].ID })
-	return acs
-}
-
-// SetAlias sets account alias and returns the updated info.
-func (d *Directory) SetAlias(id, alias string) *Info {
-	if !isAccountID(id) {
-		return nil
-	}
-	if d.aliases == nil {
-		d.aliases = make(map[string]string)
-	}
-	d.aliases[id] = alias
-	d.applyAliases()
-	return d.accounts[id]
-}
-
-// applyAliases updates Info.Alias fields using current account aliases map.
-func (d *Directory) applyAliases() {
-	if d.accounts == nil && len(d.aliases) > 0 {
-		d.accounts = make(map[string]*Info, len(d.aliases))
-	}
-	for id, alias := range d.aliases {
-		ac := d.accounts[id]
-		if ac == nil {
-			ac = &Info{ID: id}
-			d.accounts[id] = ac
-		}
-		ac.Alias = alias
-	}
-}
-
-// isAccountID returns true if id is a valid AWS account ID. Copied from awsx to
-// avoid import cycle.
-func isAccountID(id string) bool {
-	if len(id) != 12 {
-		return false
-	}
-	for i := 11; i >= 0; i-- {
-		if c := id[i]; c < '0' || '9' < c {
-			return false
-		}
-	}
-	return true
+// noOrg returns true if the AWS Organizations API is not available in the
+// current partition.
+func (d *Directory) noOrg() bool {
+	p := region.Partition(d.Client.Config.Region)
+	return region.Subset(p, orgs.ServiceName) == nil
 }
--- oktapus/account/account_test.go
+++ oktapus/account/account_test.go
@@ -8,44 +8,25 @@ import (
 
 	"github.com/LuminalHQ/cloudcover/x/awsmock"
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/aws/awserr"
 	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
 	orgs "github.com/aws/aws-sdk-go-v2/service/organizations"
-	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestDirectory(t *testing.T) {
-	cfg := awsmock.Config(func(q *aws.Request) {
-		switch out := q.Data.(type) {
-		case *sts.GetCallerIdentityOutput:
-			*out = sts.GetCallerIdentityOutput{
-				Account: aws.String("000000000000"),
-			}
-		case *orgs.DescribeOrganizationOutput:
-			out.Organization = &orgs.Organization{
-				MasterAccountId: aws.String("000000000000"),
-			}
-		case *orgs.ListAccountsOutput:
-			out.Accounts = []orgs.Account{
-				{Id: aws.String("000000000000"), Name: aws.String("master")},
-				{Id: aws.String("000000000001"), Name: aws.String("test1")},
-			}
-		default:
-			panic("unsupported api: " + q.Operation.Name)
-		}
-	})
-	d := NewDirectory(&cfg)
-	require.NoError(t, d.Refresh())
-	assert.Equal(t, Org{MasterID: "000000000000"}, d.Org())
-	want := []*Info{
-		{ID: "000000000000", Name: "master"},
-		{ID: "000000000001", Name: "test1"},
-	}
-	assert.Equal(t, want, d.Accounts())
+func TestIsID(t *testing.T) {
+	assert.False(t, IsID(""))
+	assert.False(t, IsID("123456789/12"))
+	assert.False(t, IsID("123456789:12"))
+	assert.True(t, IsID("123456789012"))
+}
 
-	// Aliases
+func TestIsErrorNoOrg(t *testing.T) {
+	assert.False(t, IsErrorNoOrg(nil))
+	assert.True(t, IsErrorNoOrg(errNoOrg))
+}
+
+func TestLoadAliases(t *testing.T) {
 	tmp, err := ioutil.TempFile("", "account_test.")
 	require.NoError(t, err)
 	defer func() {
@@ -56,67 +37,64 @@ func TestDirectory(t *testing.T) {
 		"# comment",
 		"   ",
 		"aws  000000000000  main",
-		"aws\t000000000002\ttest2",
-		"aws-us-gov 000000000003 test3",
+		"aws\t000000000001\ttest1",
+		"aws-us-gov 000000000002 test2",
 		"",
 	}, "\n"))
 	require.NoError(t, tmp.Close())
-	require.NoError(t, d.LoadAliases(tmp.Name()))
 
-	want = []*Info{
-		{ID: "000000000000", Name: "master", Alias: "main"},
-		{ID: "000000000001", Name: "test1"},
-		{ID: "000000000002", Alias: "test2"},
+	have, err := LoadAliases(tmp.Name(), endpoints.AwsPartitionID)
+	assert.NoError(t, err)
+	want := map[string]string{
+		"000000000000": "main",
+		"000000000001": "test1",
 	}
-	assert.Equal(t, want, d.Accounts())
-	assert.Equal(t, "main", want[0].DisplayName())
-	assert.Equal(t, "test1", want[1].DisplayName())
-	assert.Equal(t, "test2", want[2].DisplayName())
+	assert.Equal(t, want, have)
 
-	cfg.Region = endpoints.UsGovWest1RegionID
-	d = NewDirectory(&cfg)
-	require.NoError(t, d.LoadAliases(tmp.Name()))
-	assert.Equal(t, []*Info{{ID: "000000000003", Alias: "test3"}}, d.Accounts())
+	have, err = LoadAliases(tmp.Name(), endpoints.AwsUsGovPartitionID)
+	assert.NoError(t, err)
+	want = map[string]string{"000000000002": "test2"}
+	assert.Equal(t, want, have)
 
-	assert.Equal(t, &Info{ID: "000000000004"}, d.SetAlias("000000000004", ""))
-	want = []*Info{
-		{ID: "000000000003", Alias: "test3"},
-		{ID: "000000000004"},
-	}
-	assert.Equal(t, want, d.Accounts())
+	have, err = LoadAliases(tmp.Name(), endpoints.AwsCnPartitionID)
+	assert.NoError(t, err)
+	assert.Nil(t, have)
+}
 
-	// Errors
-	assert.Equal(t, ErrNoOrg, d.Refresh())
-
-	cfg = awsmock.Config(func(q *aws.Request) {
+func TestDirectory(t *testing.T) {
+	cfg := awsmock.Config(func(q *aws.Request) {
 		switch out := q.Data.(type) {
-		case *sts.GetCallerIdentityOutput:
-			*out = sts.GetCallerIdentityOutput{
-				Account: aws.String("000000000000"),
-			}
-		case *orgs.DescribeOrganizationOutput:
-			q.Error = awserr.New(orgs.ErrCodeAWSOrganizationsNotInUseException, "", nil)
-		default:
-			panic("unsupported api: " + q.Operation.Name)
-		}
-	})
-	d = NewDirectory(&cfg)
-	assert.Equal(t, ErrNoOrg, d.Init())
-	assert.Equal(t, ErrNoOrg, d.Refresh())
-
-	cfg = awsmock.Config(func(q *aws.Request) {
-		switch out := q.Data.(type) {
-		case *sts.GetCallerIdentityOutput:
-			*out = sts.GetCallerIdentityOutput{
-				Account: aws.String("000000000001"),
-			}
 		case *orgs.DescribeOrganizationOutput:
 			out.Organization = &orgs.Organization{
 				MasterAccountId: aws.String("000000000000"),
 			}
+		case *orgs.ListAccountsOutput:
+			if q.Params.(*orgs.ListAccountsInput).NextToken == nil {
+				out.Accounts = []orgs.Account{
+					{Id: aws.String("000000000000"), Name: aws.String("master")},
+				}
+				out.NextToken = aws.String("1")
+			} else {
+				out.Accounts = []orgs.Account{
+					{Id: aws.String("000000000001"), Name: aws.String("test1")},
+				}
+			}
 		default:
 			panic("unsupported api: " + q.Operation.Name)
 		}
 	})
-	assert.Equal(t, ErrNotMaster, NewDirectory(&cfg).Refresh())
+	d := NewDirectory(&cfg)
+	require.NoError(t, d.Init())
+	assert.Equal(t, Org{MasterID: "000000000000"}, d.Org)
+
+	require.NoError(t, d.Refresh())
+	want := map[string]*Info{
+		"000000000000": {ID: "000000000000", Name: "master"},
+		"000000000001": {ID: "000000000001", Name: "test1"},
+	}
+	assert.Equal(t, want, d.Accounts)
+
+	d.Client.Config.Region = endpoints.UsGovWest1RegionID
+	assert.Equal(t, errNoOrg, d.Init())
+	assert.Equal(t, errNoOrg, d.Refresh())
 }
